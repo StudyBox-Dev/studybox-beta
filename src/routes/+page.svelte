e2e506a9e92629b1e35c7a018fbecac03b7bf2a0
wlankasper <asu.artur.smirnov@gmail.com>
2023-01-23 21:30:30 +0100

    beta heading section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,48 @@
-<h2>StudyBox</h2>
-<a href="/sign-up/">Sign Up</a>
-<a href="/sign-in/">Sign In</a>
-<a href="/settings/">Settings</a>
-<a href="/boxes/">boxes</a>
-<a href="/box/">box</a>
+<div class="section section-heading flex flex-align-center flex-justify-center flex-row">
+    <div class="heading-text flex flex-column">
+        <h1>
+            <span class="color-accent">STUDY</span><span class="color-accent-extend">BOX</span>
+        </h1>
+        <h3 class="tagline color-main">Let's study something cool together <span class="dot dot-min color-main">.</span><span class="dot dot-mid color-accent">.</span><span class="dot dot-max color-accent-extend">.</span></h3>
+    </div>
+    <div class="heading-img-box" />
+    <img src="/box-light.svg" alt="logo (box glowing from the inside)" srcset="">
+</div>
+<div class="section section-description flex flex-align-center flex-justify-center flex-row">
+    <div class="description-text">
+        <div class="text-block flex flex-row">
+            <h2>
+                <span class="color-accent-extend">B</span>OX
+            </h2>
+            <h4 class="color-main">
+                A space in which materials, manuals and personal notes on a
+                specific topic are collected.
+            </h4>
+        </div>
+        <div class="text-block flex flex-row">
+            <h2>
+                <span class="color-accent-extend">W</span>HY
+            </h2>
+            <h4 class="color-main">
+                So that we share knowledge with each other and develop in
+                interesting areas without borders.
+            </h4>
+        </div>
+    </div>
+    <div class="description-img-box" />
+</div>
+
+<style>
+div.section {
+    height: 100vh;
+    gap: 4vw;
+}
+
+div.heading-text {
+    gap: 30px;
+}
+
+h3.tagline {
+    width: 450px;
+}
+</style>
